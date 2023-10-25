84a50e995a4cf3d60b2a6a47061c966354a58018
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-25 02:18:26 -0300

    04-A bit of routing
    04-A bit of routing

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const http = require('http');
-// const fs = require('fs');
-
-// let HTML = fs.readFileSync(`${__dirname}/index.html`)
+const fs = require('fs');
 
 const names = ['francis','james','rob'];
 const cars = {
@@ -9,13 +7,22 @@ const cars = {
     model:"Fiesta"
 }
 
-const server = http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'application/json'});
-    const json = JSON.stringify({
-        names,
-        cars
-    });
-    res.end(json)
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        let HTML = fs.readFileSync(`${__dirname}/index.html`)
+        res.end(HTML)
+    } else if(req.url === '/api/user'){
+        res.writeHead(200,{'Content-Type':'application/json'});
+        const json = JSON.stringify({
+            names,
+            cars
+        });
+        res.end(json)
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 })
 
 server.listen(8181,'127.0.0.1');
