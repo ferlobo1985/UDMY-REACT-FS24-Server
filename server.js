004938eeacef6f2751b546748c553e8198c66774
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-25 02:10:48 -0300

    03-JSON response

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
-let HTML = fs.readFileSync(`${__dirname}/index.html`)
+// let HTML = fs.readFileSync(`${__dirname}/index.html`)
+
+const names = ['francis','james','rob'];
+const cars = {
+    name:"Ford",
+    model:"Fiesta"
+}
 
 const server = http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(HTML);
+    res.writeHead(200,{'Content-Type':'application/json'});
+    const json = JSON.stringify({
+        names,
+        cars
+    });
+    res.end(json)
 })
 
 server.listen(8181,'127.0.0.1');
